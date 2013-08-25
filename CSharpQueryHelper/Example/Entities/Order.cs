64020789dae3037610bf5a5a6a23714925c3b12c
ctigeek <ctigeek@gmail.com>
2013-08-25 19:51:23 -0400

    Sample code!

--- CSharpQueryHelper/Example/Entities/Order.cs
+++ CSharpQueryHelper/Example/Entities/Order.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace CSharpQueryHelper.Example
 {
-    public enum OrderStatus {
-        Ordered,
-        BackOrdered,
-        Shipped,
-        Returned,
-        Lost,
-        Forgotten,
-        Destroyed,
-        Stolen
+    public enum OrderStatus
+    {
+        Ordered = 1,
+        BackOrdered = 2,
+        Shipped = 3,
+        Returned = 4,
+        Lost = 5,
+        Forgotten = 6,
+        Destroyed = 7,
+        Stolen = 8
     }
 
     public class Order
