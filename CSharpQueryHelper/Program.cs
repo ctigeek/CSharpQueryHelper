af62fc7b87796d50014f2b842a831e8fe6d40d6d
ctigeek <ctigeek@gmail.com>
2014-04-15 17:05:51 -0400

    More example code!

--- CSharpQueryHelper/Program.cs
+++ CSharpQueryHelper/Program.cs
@@ -27,58 +27,12 @@ namespace CSharpQueryHelper
                     parent.ChildObjects.Add(child);
                 }
 
-                var queryList = new List<SQLQuery>();
+                parent.SaveParent();
 
-                var parentQuery = parent.GetInsertQuery();
-                parentQuery.GroupNumber = 1;
-                parentQuery.OrderNumber = 1;
-                int parentKey = 0;
-                parentQuery.PostQueryProcess = q =>
-                {
-                    parentKey = parentQuery.ReturnValue;
-                    return true;
-                };
-                queryList.Add(parentQuery);
-                int orderNumber =1;
-                foreach (var child in parent.ChildObjects)
-                {
-                    var childQuery = child.GetInsertQuery(q =>
-                    {
-                        q.Parameters.Add("parentpk", parentKey);
-                    });
-                    childQuery.GroupNumber = 2;
-                    childQuery.OrderNumber = orderNumber;
-                    orderNumber++;
-                    queryList.Add(childQuery);
-                }
-                
-                var qh = new QueryHelper("sandbox");
-                qh.RunQuery(queryList, true);
+                var parents = Extensions.LoadParents();
 
 
 
-                
-                //var sql = "select someString from table1 where pk=9;";
-
-                //var scalerQuery = new SQLQueryScaler<string>(sql);
-                
-                
-                
-                
-                
-                //var query = new SQLQuery(sql, SQLQueryType.DataReader);
-
-                //query.ProcessRow = new Func<System.Data.Common.DbDataReader, bool>(dr =>
-                //{
-                //    Console.WriteLine("pk={0}  someString={1}  someFK={2}", dr[0], dr[1], dr[2]);
-                //    return true;
-                //});
-                
-
-                //var someString = qh.RunScalerQuery<string>(sql);
-                //Console.WriteLine(someString);
-
-                //qh.RunQuery(query);
             }
             catch (Exception ex)
             {
@@ -104,6 +58,80 @@ namespace CSharpQueryHelper
 
     public static class Extensions
     {
+        public static List<ParentObject> LoadParents()
+        {
+            var parentDict = new Dictionary<int, ParentObject>();
+            var sql = @"select Parent.pk as ParentPk,
+                                Parent.Name as ParentName,
+                                Parent.SomeProp as ParentSomeProp,
+                                Child.pk as ChildPk,
+                                Child.Name as ChildName,
+                                Child.SomeProp as ChildSomeProp
+                            from Parent inner join Child on (Parent.pk = Child.parent_pk)";
+            var query = new SQLQuery(sql, SQLQueryType.DataReader);
+            query.ProcessRow = dr =>
+            {
+                //add parent
+                int parentpk = (int)dr["ParentPk"];
+                if (!parentDict.ContainsKey(parentpk))
+                {
+                    var newParentObject = new ParentObject()
+                    {
+                        ChildObjects = new List<ChildObject>(),
+                        Name = (string)dr["ParentName"],
+                        SomeProp = (int)dr["ParentSomeProp"]
+                    };
+                    parentDict.Add(parentpk, newParentObject);
+                }
+                var parentObject = parentDict[parentpk];
+                
+                //add child
+                var newChildObject = new ChildObject()
+                {
+                    Name = (string)dr["ChildName"],
+                    SomeProp = (int)dr["ChildSomeProp"]
+                };
+                parentObject.ChildObjects.Add(newChildObject);
+
+                return true;
+            };
+
+            var qh = new QueryHelper("sandbox");
+            qh.RunQuery(query);
+
+            return parentDict.Values.ToList();
+        }
+        public static void SaveParent(this ParentObject parent)
+        {
+            var queryList = new List<SQLQuery>();
+
+            var parentQuery = parent.GetInsertQuery();
+            parentQuery.GroupNumber = 1;
+            parentQuery.OrderNumber = 1;
+            int parentKey = 0;
+            parentQuery.PostQueryProcess = q =>
+            {
+                parentKey = parentQuery.ReturnValue;
+                return true;
+            };
+            queryList.Add(parentQuery);
+            int orderNumber = 1;
+            foreach (var child in parent.ChildObjects)
+            {
+                var childQuery = child.GetInsertQuery(q =>
+                {
+                    q.Parameters.Add("parentpk", parentKey);
+                });
+                childQuery.GroupNumber = 2;
+                childQuery.OrderNumber = orderNumber;
+                orderNumber++;
+                queryList.Add(childQuery);
+            }
+
+            var qh = new QueryHelper("sandbox");
+            qh.RunQuery(queryList, true);
+        }
+
         public static SQLQuery GetInsertQuery(this ChildObject childObject, Action<SQLQuery> preQuery)
         {
             string sql = "insert into Child (parent_pk, Name, SomeProp) values (@parentpk,@name,@someprop);";
@@ -124,5 +152,23 @@ namespace CSharpQueryHelper
         }        
     }
 
+/*
+CREATE TABLE [dbo].[Child](
+	[pk] [int] IDENTITY(1,1) NOT NULL,
+	[parent_pk] [int] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[SomeProp] [int] NOT NULL,
+ CONSTRAINT [PK_Chile] PRIMARY KEY CLUSTERED 
+ (	[pk] ASC)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+ ) 
+
+ CREATE TABLE [dbo].[Parent](
+	[pk] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[SomeProp] [int] NOT NULL,
+ CONSTRAINT [PK_Parent] PRIMARY KEY CLUSTERED 
+ ([pk] ASC)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+ )
+*/
 
 }
